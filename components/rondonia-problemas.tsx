f500c7be0033dd5323040f4a68257bf1ff8c1b38
emanaproducoes-cmyk <emanaproducoes@gmail.com>
2026-08-17 19:53:05 -0400

    Update rondonia-problemas.tsx

--- components/rondonia-problemas.tsx
+++ components/rondonia-problemas.tsx
@@ -5,7 +5,7 @@ import { Truck, FileStack, Building2, ShieldAlert } from 'lucide-react'
 
 const DESAFIOS = [
   {
-    icon: Broken Bridge,
+    icon: MapPinOff,
     titulo: 'Escoamento Comprometido',
     desc: 'Gargalos logísticos severos na malha rodoviária elevam o custo do frete e reduzem a competitividade do produtor rondoniense.',
   },
